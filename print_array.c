e08d1326bf0acc2c56cf8882cf2a2013a4f778da
henryno111 <h.agukwe@yahoo.com>
2023-06-22 07:55:17 -0700

    completion of task 0

---
+++ print_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t index = 0;
+
+	while (array && index < size)
+	{
+		if (index > 0)
+			printf(", ");
+		printf("%d", array[index]);
+		++index;
+	}
+	printf("\n");
+}
